d8da8a24935b1c4eec879f58f67e5296360a8482
Jared Klein <jaredk@knights.ucf.edu>
2019-10-19 23:49:31 -0400

    commit

--- klein_p2/src/Application.java
+++ klein_p2/src/Application.java
@@ -8,13 +8,13 @@ public class Application {
 
         saver1.modifyInterestRate(.04);
         saver2.modifyInterestRate(.04);
-        for (int i = 0; i < 11; i++) {
+        for (int i = 0; i < 12; i++) {
             saver1.calculateMonthlyInterest();
             saver2.calculateMonthlyInterest();
         }
 
-        System.out.println("Saver1 balance is: " + saver1.getBalance());
-        System.out.println("Saver2 balance is: " + saver2.getBalance());
+        System.out.println("Saver1 balance for 4% is: " + saver1.getBalance());
+        System.out.println("Saver2 balance for 4% is: " + saver2.getBalance());
 
         saver1.modifyInterestRate(.05);
         saver2.modifyInterestRate(.05);
