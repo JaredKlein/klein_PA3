40b614b9030352b663be65e0c3f4d1c84e0b3fb0
Jared Klein <jaredk@knights.ucf.edu>
2019-11-02 20:48:23 -0400

    come on

--- klein_p1/src/Application.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-public class Application {
-    public static void main(String[] args) throws IOException {
-        DuplicateRemover duplicateRemover = new DuplicateRemover();
-        duplicateRemover.remove("problem1.txt");
-        duplicateRemover.write("unique_words.txt");
-    }
-}
